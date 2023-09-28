837edb94bce118a16f45ce3351e0b38f29aab0ea
Marta Pintos <martapintoslopez@gmail.com>
2023-09-28 18:49:27 +0200

    Option one to set up meaning of numbers

--- piedrapapeltijera.py
+++ piedrapapeltijera.py
@@ -8,9 +8,18 @@ print("""Cuento hasta tres y escribes que quieres: piedra, papel o tijera.
 print("¿List@? (y/n)")
 input(">")
 
+def whatisit(election):
+    if election == 1:
+        print("Piedra")
+    elif election == 2:
+        print("Papel")
+    elif election == 3:
+        print("Tijera")
+
 def computerelection():
     new_election = int(random.randint(1,3))
     print(new_election)
+
 
 
 computerelection()
