c8ae954a2d87010ee1631160f30d6586b0103e80
Marta Pintos <martapintoslopez@gmail.com>
2023-09-28 18:30:45 +0200

    What to do

--- piedrapapeltijera.py
+++ piedrapapeltijera.py
@@ -3,3 +3,8 @@ import random
 print("¿Quieres jugar a piedra, papel o tijera? (y/n)")
 input(">")
 
+print("""Cuento hasta tres y escribes que quieres: piedra, papel o tijera.
+      Los dos sacaremos a la vez que hemos elegido.""")
+print("¿List@? (y/n)")
+input(">")
+
